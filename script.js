7e954615b5ed891a2d13629ff9c72f52671b0c4c
Andy <ashishdeb1231@gmail.com>
2023-03-10 13:29:08 +0530

    Re-doing everything

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('button').addEventListener('click', ()=>{
-    document.querySelector('.loon').classList.add('loon-anime')
-    setTimeout(()=>{
-        document.querySelector('.loon').classList.add('hide')
-    },4950)
-})
